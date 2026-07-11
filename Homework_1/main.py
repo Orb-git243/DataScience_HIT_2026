501be38ba8dd505d8fdf07606367ed551cb74d25
ORb-git243 <or.bareket.studies@gmail.com>
2026-07-11 18:14:06 +0300

    test_ex1

---
+++ Homework_1/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# test ex_1
+def say_hello(name):
+    return f"Hello {name}, my code is working and synchronized with GITHUB!"
+
+result = say_hello("OR")
+print("")
+print(result)
+print("")
